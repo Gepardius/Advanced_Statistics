60ca6d51f2a62a4f59a73e5a83694d8504b4be6e
Gepardius <gal.bordelius@gmail.com>
2022-04-02 10:14:01 +0200

    started 2. a)

--- Workbook.py
+++ Workbook.py
@@ -103,7 +103,6 @@ def probability_density_function(a, b):
 
     # plot histogram
     plt.clf()
-    # plt.step(np.arange(119, 241, 1), minute_probabilities, color="blue")
     plt.bar(np.arange(119, 241, 1), minute_probabilities, color="blue", fill=False, width=1, label="prob. histogram")
     plt.plot(np.arange(119, 241, 1), minute_probabilities, color="green", label="probability function")
 
@@ -122,5 +121,32 @@ def probability_density_function(a, b):
     plt.show()
 
 
+# probability_density_function(0.83, 0.16)
 
-probability_density_function(0.83, 0.16)
+# 2.
+# a) Calculate the sample covariance as well as the sample’s expectations (mean) and variances of 𝑋 and 𝑌.
+# the expected value is the sum of: [(each of the possible outcomes) × (the probability of the outcome occurring)]
+values = """(−1.202, 563.024), (2.112, 291.072), (2.827, −893.619), (−0.314, 1321.814),
+(−1.477, −91.573), (−6.516, 446.336), (−0.920, −111.487), (3.477, −153.165),
+(−7.273, 1076.221), (2.251, 477.931), (−0.713, 909.696), (−0.853, 226.865),
+(−3.176, 389.413), (1.913, −47.169), (−1.070, −178.695), (−3.385, 744.486),
+(−9.506, 362.670), (−7.004, 364.578), (0.504, 324.975), (2.861, −360.571)"""
+
+x_l = []
+for i in range(0, 40, 2):
+    x_value = values.split(",")[i]
+    x_value = x_value.replace("(", "")
+    x_value = x_value.replace("\n", "")
+    x_l.append(x_value)
+# print(x_l)
+
+y_l = []
+for i in range(1, 41, 2):
+    y_value = values.split(",")[i]
+    y_value = y_value.replace(")", "")
+    y_value = y_value.replace("\n", "")
+    y_l.append(y_value)
+# print(y_l)
+
+df = pd.DataFrame(list(zip(x_l, y_l)), columns =['X', 'Y'])
+print(df)
